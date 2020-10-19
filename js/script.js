26b52b3d9ef1f74f5904718405f39d6d183d2d3b
dicky al fattah <alfattahdicky2@gmail.com>
2020-10-20 00:52:58 +0700

    change version

--- js/script.js
+++ js/script.js
@@ -1,42 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var navbar = document.querySelectorAll('.sidenav');
+document.addEventListener('DOMContentLoaded', () => {
+	const navbar = document.querySelectorAll('.sidenav');
 	M.Sidenav.init(navbar);
 	loadNav();
 
 	function loadNav() {
-		var xhttp = new XMLHttpRequest();
+		const xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function () {
 			if (this.readyState == 4) {
 				if (this.status != 200) return;
-				document.querySelectorAll('.topnav, .sidenav').forEach(function (e) {
+				document.querySelectorAll('.topnav, .sidenav').forEach((e) => {
 					e.innerHTML = xhttp.responseText;
 				});
-				document
-					.querySelectorAll('.sidenav a, .topnav a')
-					.forEach(function (e) {
-						e.addEventListener('click', function (event) {
-							var sidenav = document.querySelector('.sidenav');
-							M.Sidenav.getInstance(sidenav).close();
+				document.querySelectorAll('.sidenav a, .topnav a').forEach((e) => {
+					e.addEventListener('click', function (event) {
+						const sidenav = document.querySelector('.sidenav');
+						M.Sidenav.getInstance(sidenav).close();
 
-							page = event.target.getAttribute('href').substr(1);
-							loadPage(page);
-						});
+						page = event.target.getAttribute('href').substr(1);
+						loadPage(page);
 					});
+				});
 			}
 		};
 		xhttp.open('GET', 'nav.html', true);
 		xhttp.send();
 	}
-	var page = window.location.hash.substr(1);
+	let page = window.location.hash.substr(1);
 	if (page == '') page = 'home';
 	loadPage(page);
 
 	function loadPage(page) {
-		var xhttp = new XMLHttpRequest();
+		let xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function () {
 			if (this.readyState == 4) {
 				if (this.status == 200) {
-					var content = document.querySelector('.body-content');
+					let content = document.querySelector('.body-content');
 					content.innerHTML = xhttp.responseText;
 					if (page === 'home') {
 						// parallax
@@ -65,7 +63,7 @@ document.addEventListener('DOMContentLoaded', function () {
 				}
 			}
 		};
-		xhttp.open('GET', 'pages/' + page + '.html', true);
+		xhttp.open('GET', `pages/${page}.html`, true);
 		xhttp.send();
 	}
 });
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'burgerIndo-v9';
-var urlsToCache = [
+const CACHE_NAME = 'burgerIndo-v12';
+let urlsToCache = [
 	'/',
 	'/nav.html',
 	'/index.html',
@@ -11,47 +11,50 @@ var urlsToCache = [
 	'/css/style.css',
 	'/js/materialize.min.js',
 	'/js/script.js',
-	'https://images.unsplash.com/photo-1561222856-eb920e4960d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-	'https://images.unsplash.com/photo-1600565193348-f74bd3c7ccdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-	'https://images.unsplash.com/photo-1555196301-ace64f260222?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-	'https://images.unsplash.com/photo-1556742212-5b321f3c261b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-	'https://images.unsplash.com/photo-1592772425601-c23646d4c119?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1056&q=80',
-	'https://images.unsplash.com/photo-1550674640-edeb406d94bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-	'https://images.unsplash.com/photo-1547584370-2cc98b8b8dc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
-	'https://images.unsplash.com/photo-1594212699903-ec8a3eca50f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
-	'https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=703&q=80',
-	'https://images.unsplash.com/photo-1550317138-10000687a72b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=793&q=80',
-	'https://images.unsplash.com/photo-1457460866886-40ef8d4b42a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-	'https://images.unsplash.com/photo-1551615593-ef5fe247e8f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=780&q=80',
-	'https://images.unsplash.com/photo-1542600176-9d2c4bb4bc1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80',
-	'https://images.unsplash.com/photo-1590955022540-06dbbe46ccda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-	'https://images.unsplash.com/photo-1457518919282-b199744eefd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80',
-	'https://images.unsplash.com/photo-1551198297-0a648941bd7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-	'https://images.unsplash.com/photo-1574012836510-ac947962fbc9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80',
-	'https://images.unsplash.com/photo-1514361726087-38371321b5cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-	'https://images.unsplash.com/photo-1423666639041-f56000c27a9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80',
-	'https://images.unsplash.com/photo-1495546968767-f0573cca821e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80',
-	'https://images.unsplash.com/photo-1515467705959-1142c6a92b19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=890&q=80',
-	'https://images.unsplash.com/photo-1602192103201-d763907bc41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-	'https://images.unsplash.com/photo-1542342656241-f23fedd6bafa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80',
-	'https://images.unsplash.com/photo-1529564879024-c54e7c2dd0e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-	'/icon.png',
+	'/js/sw-register.js',
+	'/icon-192x192.png',
+	'/icon-512x512.png',
 	'/manifest.json',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'/assets/1.png',
+	'/assets/2.png',
+	'/assets/3.png',
+	'/assets/4.png',
+	'/assets/5.png',
+	'/assets/6.png',
+	'/assets/7.png',
+	'/assets/8.png',
+	'/assets/9.png',
+	'/assets/10.png',
+	'/assets/11.png',
+	'/assets/12.png',
+	'/assets/13.png',
+	'/assets/14.png',
+	'/assets/15.png',
+	'/assets/16.png',
+	'/assets/17.png',
+	'/assets/18.png',
+	'/assets/19.png',
+	'/assets/20.png',
+	'/assets/21.png',
+	'/assets/22.png',
+	'/assets/23.png',
+	'/assets/24.png',
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', (event) => {
 	event.waitUntil(
-		caches.open(CACHE_NAME).then(function (cache) {
+		caches.open(CACHE_NAME).then((cache) => {
 			return cache.addAll(urlsToCache);
 		}),
 	);
 });
 
-self.addEventListener('activate', function (event) {
+self.addEventListener('activate', (event) => {
 	event.waitUntil(
-		caches.keys().then(function (cacheNames) {
+		caches.keys().then((cacheNames) => {
 			return Promise.all(
-				cacheNames.map(function (cacheName) {
+				cacheNames.map((cacheName) => {
 					if (cacheName !== CACHE_NAME) {
 						console.log(`ServiceWorker: cache ${cacheName} dihapus`);
 						return caches.delete(cacheName);
@@ -62,15 +65,13 @@ self.addEventListener('activate', function (event) {
 	);
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', (event) => {
 	event.respondWith(
-		caches
-			.match(event.request, { cacheName: CACHE_NAME })
-			.then(function (response) {
-				if (response) {
-					return response;
-				}
-				return fetch(event.request);
-			}),
+		caches.match(event.request, { cacheName: CACHE_NAME }).then((response) => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request);
+		}),
 	);
 });
